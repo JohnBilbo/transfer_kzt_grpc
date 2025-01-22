33b1f68e1748a029d184eae13ae37d6801981c40
JohnBilbo <magmabegzzz@gmail.com>
2025-01-22 16:41:34 +0500

    expand project

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,18 @@
 package main
 
-func main() {
+import (
+	"os"
+	"user/internal/config"
+)
 
+func main() {
+	cwd, err := os.Getwd()
+	if err != nil {
+		panic(err)
+	}
+	c, err := config.FindProjectRoot(cwd)
+	if err != nil {
+		panic(err)
+	}
+	print(c)
 }
--- internal/config/config.go
+++ internal/config/config.go
@@ -1,8 +1,15 @@
 package config
 
-import "time"
+import (
+	"fmt"
+	"github.com/spf13/viper"
+	"os"
+	"path/filepath"
+	"time"
+)
 
 type Config struct {
+	ConfigPath  string     `yaml:"config_path"`
 	StoragePath string     `yaml:"storage_path"`
 	GRPCConfig  GRPCConfig `yaml:"grpc"`
 }
@@ -12,9 +19,9 @@ type GRPCConfig struct {
 	Timeout time.Duration `yaml:"timeout"`
 }
 
-func DefaultConfig() Config {
-	return Config{
-		StoragePath: "./data",
+func DefaultConfig() *Config {
+	return &Config{
+		StoragePath: "./storage",
 		GRPCConfig: GRPCConfig{
 			Port:    "50505",
 			Timeout: 5 * time.Second,
@@ -22,10 +29,26 @@ func DefaultConfig() Config {
 	}
 }
 
-func NewConfig() Config {
-	return Config{}
+func NewConfig() *Config {
+	return &Config{
+		StoragePath: viper.GetString("app.storage_path"),
+		GRPCConfig: GRPCConfig{
+			Port:    viper.GetString("app.grpc_port"),
+			Timeout: viper.GetDuration("app.grpc_timeout"),
+		},
+	}
 }
 
-func (c *Config) setConfig() {
-
+// FindProjectRoot - функция перечисляет директории и возвращает путь, найденного корня
+func FindProjectRoot(startDir string) (string, error) {
+	for {
+		if _, err := os.Stat(filepath.Join(startDir, "documents")); !os.IsNotExist(err) {
+			return startDir, nil
+		}
+		parentDir := filepath.Dir(startDir)
+		if parentDir == startDir {
+			return "", fmt.Errorf("project root not found")
+		}
+		startDir = parentDir
+	}
 }
